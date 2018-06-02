175877d2872a8988e5ceb250c3593ea0b5350689
curren49918102 <curren49918102@gmail.com>
2018-06-02 20:58:33 +0900

    Update index.py
    Update 0602 1

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World 0602',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
